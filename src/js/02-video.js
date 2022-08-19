56e875f8d4c8bbec5fbef14a1aec3345632a3273
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-20 01:20:13 +0300

    add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+
+const onTimeUpdate = function (data) {
+    console.log(data);
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onTimeUpdate,1000));
+
+
+const playerCurrentTime = localStorage.getItem(STORAGE_KEY);
+
+if (playerCurrentTime) {
+    player.setCurrentTime(playerCurrentTime);
+}
