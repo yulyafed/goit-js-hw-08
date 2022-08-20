db37f70d67940cc7d5634e090c4f0ba55af278b4
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-20 12:42:22 +0300

    Reverted fixes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,6 @@
-import SimpleLightbox from "./simplelightbox";
-import "./simplelightbox/dist/simple-lightbox.min.css";
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 
 const galleryContainer = document.querySelector('.gallery');
 
