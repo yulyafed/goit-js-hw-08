dd6cb8aa02f3f240faaf2ac8abcba1e2d3de5b2e
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-08-20 12:08:37 +0300

    add changes in task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,31 +8,27 @@ const form = document.querySelector('.feedback-form');
 const input = document.querySelector('[name="email"]');
 const textarea = document.querySelector('[name="message"]');
 
+populateForm();
 
-
+form.addEventListener('input', throttle(onFormClick, 500));
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', throttle(onFormClick,500));
-input.addEventListener('input', onInputClick);
-textarea.addEventListener('input', onTextareaClick);
 
 function onFormClick(e) {
     formData[e.target.name] = e.target.value;
-    // console.log(formData)
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
- 
-function onFormSubmit(e) { 
-    e.preventDefault();
-    e.currentTarget.reset();
-    console.log(formData);
-    localStorage.removeItem(STORAGE_KEY);
-}
 
-function populateForm(e) { 
-    const savedMessage = localStorage.getItem(JSON.parse(STORAGE_KEY));
-    console.log(savedMessage)
-    if (savedMessage) { 
-        input.value = savedMessage.email
-        textarea.value = savedMessage.message
+function populateForm(e) {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMessage) {
+        input.value = savedMessage.email;
+        textarea.value = savedMessage.message;
     }
 }
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+}
+
